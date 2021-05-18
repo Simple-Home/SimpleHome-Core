383a11b97f942976f4d742e207cb353973d86362
JonatanRek <vasek@steelants.cz>
2021-05-18 16:36:10 +0200

    Few Tweeks

--- resources/views/devices/list.blade.php
+++ resources/views/devices/list.blade.php
@@ -7,48 +7,16 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             @if(!empty($devices) && count($devices) > 0)
-            <div class="card">
-                <div class="card-header">{{ __('Device List') }}</div>
-                <div class="table-responsive">
-                    <table class="table table-striped mb-0">
-                        <thead>
-                            <tr>
-                                <th scope="col">Hostname</th>
-                                <th scope="col">Token</th>
-                                <th scope="col">Heartbeat</th>
-                                <th scope="col">Signal</th>
-                                <th scope="col">Sleep</th>
-                                <th scope="col">Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($devices as $device)
-                            <tr>
-                                <td>{{$device->hostname}}</td>
-                                <td>{{$device->token}}</td>
-                                <td class="{{ $device->connection_error ? 'text-danger' : 'text-success' }}">{{$device->heartbeat}}</td>
-                                <td>{{$device->signal_strength}} %</td>
-                                <td>{{$device->sleep}} ms</td>
-                                <td>
-                                    <a href="/test" class="btn btn-primary"><i class="fas fa-upload"></i></a>
-                                    <a href="/test" class="btn btn-primary"><i class="fas fa-redo"></i></a>
-                                    @if ($device->approved)
-                                    <a href="/test" class="btn btn-primary"><i class="fas fa-times"></i></a>
-                                    @else
-                                    <a href="/test" class="btn btn-primary"><i class="fas fa-check"></i></a>
-                                    @endif
-                                    <a href="/test" class="btn btn-primary"><i class="fas fa-cog"></i></a>
-                                    <a href="/test" class="btn btn-primary"><i class="fas fa-terminal"></i></a>
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-                <div class="col">
-                    <div class="float-right">
-                        <!-- Button trigger modal -->
-                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">+</button>
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col">
+                        <h2>{{ __('Device List') }}</h2>
+                    </div>
+                    <div class="col">
+                        <div class="float-right">
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">+</button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -60,6 +28,7 @@
                             <th>Token</th>
                             <th>Heartbeat</th>
                             <th>Sleep</th>
+                            <th>Signal</th>
                             <th style="width: 25%">Actions</th>
                         </tr>
                     </thead>
@@ -70,6 +39,7 @@
                             <td>{{$device->token}}</td>
                             <td class="{{ $device->connection_error ? 'text-danger' : 'text-success' }}">{{$device->heartbeat}}</td>
                             <td>{{$device->sleep}} ms</td>
+                            <td>{{$device->signal_strength}}%</td>
                             <td>
                                 <a href="/test" class="btn btn-primary"><i class="fas fa-upload"></i></a>
                                 <a href="/test" class="btn btn-primary"><i class="fas fa-redo"></i></a>
