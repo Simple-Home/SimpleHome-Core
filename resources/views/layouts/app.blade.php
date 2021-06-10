aa699a66cf6d7a88d8e7b9d9a5d7ea1b4b6a7061
JonatanRek <gamerclassn7@gmail.com>
2021-06-10 17:18:24 +0200

    Dark mode detection speed up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,11 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/dark-mode-switch.min.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -116,8 +119,4 @@
         </main>
     </div>
 </body>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('js/dark-mode-switch.min.js') }}" defer></script>
 </html>
