2a9e88ba50d4145b84f77052fb8da0a5e3896e71
Brad Sanders <thebradleysanders@users.noreply.github.com>
2021-06-12 00:17:46 -0500

    More  Fixes to Modules

--- Modules/ExampleBinding/Http/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web', 'prefix' => 'exampleLight', 'namespace' => 'Modules\ExampleBinding\Http\Controllers'], function()
-{
-    Route::get('/', 'ExampleBindingController@index');
-});
--- Modules/ExampleBinding/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('exampleLight::layouts.master')
-
-@section('content')
-    <h1>Example Module</h1>
-
-    <p>
-        This view is loaded from module: {!! config('exampleLight.name') !!}
-    </p>
-@stop
