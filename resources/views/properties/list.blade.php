a8403fa886e306454b53abb8e3d9f1cb3749e80c
JonatanRek <gamerclassn7@gmail.com>
2021-06-18 14:04:55 +0200

    Bug Fix

--- resources/views/properties/list.blade.php
+++ resources/views/properties/list.blade.php
@@ -38,18 +38,22 @@
                             </div>
                         </div>
                         <div class="row">
-                            @if ($property->last_value->value == "off")
-                                <button type="button" onclick="deviceControl('{{ $property->device->hostname }}', '{{ $property->id }}', 'state', 'on');" class="btn btn-success">Turn On</button>
-                            @else
-                                <button type="button" onclick="deviceControl('{{ $property->device->hostname }}', '{{ $property->id }}', 'state', 'off');" class="btn btn-danger">Turn Off</button>
-                            @endif
+                            @if (!empty($property->last_value->value) && ($property->type == "switch"))
+								@if (strtolower($property->last_value->value) == "off")
+									<button type="button" onclick="deviceControl('{{ $property->device->hostname }}', '{{ $property->id }}', 'state', 'on');" class="btn btn-success">Turn On</button>
+								@else
+									<button type="button" onclick="deviceControl('{{ $property->device->hostname }}', '{{ $property->id }}', 'state', 'off');" class="btn btn-danger">Turn Off</button>
+								@endif
+							@endif
+							
                             &nbsp;
-                            @if ($property->type == "light")
+                            @if (strtolower($property->type) == "light")
                                 <button type="button" onclick="deviceControl('{{ $property->device->hostname }}', '{{ $property->id }}', 'brightness', '10');" class="btn btn-primary">Max Brightness</button>
                                 &nbsp;
                                 <button type="button" onclick="deviceControl('{{ $property->device->hostname }}', '{{ $property->id }}', 'brightness', '1');" class="btn btn-primary">Min Brightness</button>
                             @endif
-                            @if ($property->type == "speaker")
+							
+                            @if (strtolower($property->type) == "speaker")
                                 <button type="button" onclick="deviceControl('{{ $property->device->hostname }}', '{{ $property->id }}', 'volume', '10');" class="btn btn-primary">Max Volume</button>
                                 &nbsp;
                                 <button type="button" onclick="deviceControl('{{ $property->device->hostname }}', '{{ $property->id }}', 'volume', '1');" class="btn btn-primary">Min Volume</button>
