edd9c1dff0ddbea5231cf1d6b743528555c34b56
JonatanRek <gamerclassn7@gmail.com>
2021-06-23 13:15:10 +0200

    Tweeks

--- resources/views/settings/components/subnavigation.blade.php
+++ resources/views/settings/components/subnavigation.blade.php
@@ -3,15 +3,15 @@
         <a class="nav-link" href="{{ route('server_info') }}">{{ __('Dashboard') }}</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="{{ route('server_info') }}">{{ __('Houskeeping') }}</a>
+        <a class="nav-link disabled" href="{{ route('server_info') }}">{{ __('Houskeeping') }}</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="{{ route('server_info') }}">{{ __('Backups') }}</a>
+        <a class="nav-link disabled" href="{{ route('server_info') }}">{{ __('Backups') }}</a>
     </li>
     <li class="nav-item">
         <a class="nav-link" href="{{ route('modules_list') }}">{{ __('Modules') }}</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="{{ route('server_info') }}">{{ __('Logs') }}</a>
+        <a class="nav-link disabled" href="{{ route('server_info') }}">{{ __('Logs') }}</a>
     </li>
 </ul>
