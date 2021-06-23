203fca0e164878ea97ecd4958ca4d102379c7f4b
JonatanRek <gamerclassn7@gmail.com>
2021-06-23 12:08:27 +0200

    Log Menu Item

--- resources/views/settings/components/subnavigation.blade.php
+++ resources/views/settings/components/subnavigation.blade.php
@@ -11,4 +11,7 @@
     <li class="nav-item">
         <a class="nav-link" href="{{ route('modules_list') }}">{{ __('Modules') }}</a>
     </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('server_info') }}">{{ __('Logs') }}</a>
+    </li>
 </ul>
