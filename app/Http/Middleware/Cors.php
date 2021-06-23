43f4a639fbc115f5c927b97521fe832defc42fd5
christoph <mastox@live.de>
2021-06-23 15:38:10 +0200

    WIP: backup - add pavel-mironchik/laravel-backup-panel - spatie/laravel-backup

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,6 +16,10 @@ class Cors
      */
     Public function handle($request, Closure $next)
     {
+        /*
+         * @TODO Call to undefined method Symfony\Component\HttpFoundation\BinaryFileResponse::header()
+         * http://localhost/livewire/livewire.js?id=e6704f81026a73a52725
+        */
       return $next($request)
        ->header('Access-Control-Allow-Origin', '*')
        ->header('Access-Control-Allow-Methods',
