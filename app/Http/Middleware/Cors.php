bd299046ea31c27aea6cccd7d80c6508284bee70
christoph <mastox@live.de>
2021-06-24 09:43:55 +0200

    fix backup dashboard - fix Call to undefined method Symfony\Component\HttpFoundation\BinaryFileResponse::header()

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -10,21 +10,16 @@ class Cors
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
-    Public function handle($request, Closure $next)
+    public function handle($request, Closure $next)
     {
-        /*
-         * @TODO Call to undefined method Symfony\Component\HttpFoundation\BinaryFileResponse::header()
-         * http://localhost/livewire/livewire.js?id=e6704f81026a73a52725
-        */
-      return $next($request)
-       ->header('Access-Control-Allow-Origin', '*')
-       ->header('Access-Control-Allow-Methods',
-                 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-       ->header('Access-Control-Allow-Headers',
-                'Content-Type, Authorization, X-Requested-With, X-XSRF-TOKEN');
-}
+        $response = $next($request);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-XSRF-TOKEN');
+        return $response;
+    }
 }
