64b3e6b4422632f299a4744cab13486184aa61fb
JonatanRek <gamerclassn7@gmail.com>
2021-08-19 17:22:54 +0200

    Another Tweeks

--- resources/views/properties/components/controls.blade.php
+++ resources/views/properties/components/controls.blade.php
@@ -1,10 +1,8 @@
 @if (!empty($property->last_value->value) && ($property->type == "switch"))
 @if (strtolower($property->last_value->value) == "off")
 <button type="button" onclick="deviceControl('{{ $property->device->hostname }}', '{{ $property->id }}', 'state', 'on');" class="btn btn-success">Turn On</button>
-<a href="{{-- route('device.control', [$device->hostname, "state", "ON"]) --}}" title="On" class="btn btn-primary"><i class="fas fa-power-off"></i></a>
 @else
 <button type="button" onclick="deviceControl('{{ $property->device->hostname }}', '{{ $property->id }}', 'state', 'off');" class="btn btn-danger">Turn Off</button>
-<a href="{{-- route('device.control', [$device->hostname, "state", "OFF"]) --}}" title="Off" class="btn btn-danger"><i class="fas fa-power-off"></i></a>
 @endif
 @endif
 
@@ -19,3 +17,6 @@
 &nbsp;
 <button type="button" onclick="deviceControl('{{ $property->device->hostname }}', '{{ $property->id }}', 'volume', '1');" class="btn btn-primary">Min Volume</button>
 @endif
+
+<a href="{{ route('device.control', [$property->device->hostname , "batt", "ON"]) }}" title="On" class="btn btn-primary"><i class="fas fa-power-off"></i></a>
+<a href="{{ route('device.control', [$property->device->hostname , "batt", "OFF"]) }}" title="Off" class="btn btn-danger"><i class="fas fa-power-off"></i></a>
