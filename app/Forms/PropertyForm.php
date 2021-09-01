2ab5452ebd1a761926fd60a15e10aa0df581b828
haitem <silentislp@gmail.com>
2021-09-01 18:20:58 +0200

    property form

---
+++ app/Forms/PropertyForm.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Forms;
+
+use Kris\LaravelFormBuilder\Form;
+
+class PropertyForm extends Form
+{
+    public function buildForm()
+    {
+        $this
+            ->add('nick_name', Field::TEXT, [
+                'rules' => 'required',
+                'label' => "Nick name"
+            ])
+            ->add('icon', Field::BUTTON_BUTTON, [
+                'attr' => ['class' => 'btn btn-secondary iconButton', 'id' => 'icon', 'role' => 'iconpicker', 'data-icon' => (!empty($this->getData('icon')) ? $this->getData('icon') : "")],
+            ])
+            ->add('history', Field::NUMBER, [
+                'rules' => 'required',
+                'label' => "History"
+            ])
+            ->add('units', Field::TEXT, [
+                'rules' => 'required',
+                'label' => "Units"
+            ])
+            ->add('room_id', Field::SELECT, [
+                'choices' => (!empty($this->getData('rooms')) ? $this->getData('rooms') : array(0 => "Nothing")),
+                'rules' => 'required',
+                'label' => "Room"
+            ])
+            ->add('saveProfile', Field::BUTTON_SUBMIT, [
+                'label' => __('simplehome.save')
+            ]);
+    }
+}
