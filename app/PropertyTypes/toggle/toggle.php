6a7ddf5ba32a6aed76bfa2fec2ba4dfd00ad84df
GamerClassN7 <gamerclassn7@gmail.com>
2021-09-20 22:30:04 +0200

    Typo Fixes

--- app/PropertyTypes/toggle/toggle.php
+++ app/PropertyTypes/toggle/toggle.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\PropertyTypes\toggle;
+namespace App\PropertyTypes\Toggle;
 use App\PropertyTypes\PropertyTypes;
 
-abstract class toggle extends PropertyTypes
+abstract class Toggle extends PropertyTypes
 {
     abstract public function state($value);
     public function allowedValues(){ return array("state"=>array("on","off")); }
