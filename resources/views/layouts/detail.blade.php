793ff786c64a449d2625e8d914119184b3890b77
JonatanRek <vasek@steelants.cz>
2021-10-18 09:51:08 +0200

    Detail LAyou Tweaks and Fixes

--- resources/views/layouts/detail.blade.php
+++ resources/views/layouts/detail.blade.php
@@ -10,21 +10,30 @@
     <title>@yield('title') - {{ config('app.name', 'Simple Home') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset(mix('js/manifest.js'), (Request::server('HTTP_X_FORWARDED_PROTO') != 'http' ? true : '')) }}" defer></script>
-    <script src="{{ asset(mix('js/vendor.js'), (Request::server('HTTP_X_FORWARDED_PROTO') != 'http' ? true : '')) }}" defer></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" defer></script>
+    <script src="{{ asset(mix('js/manifest.js'), Request::server('HTTP_X_FORWARDED_PROTO') != 'http' ? true : '') }}">
+    </script>
+    <script src="{{ asset(mix('js/vendor.js'), Request::server('HTTP_X_FORWARDED_PROTO') != 'http' ? true : '') }}">
+    </script>
+    <script src="{{ asset(mix('js/app.js'), Request::server('HTTP_X_FORWARDED_PROTO') != 'http' ? true : '') }}">
+    </script>
+    <script
+        src="{{ asset(mix('js/notifications.js'), Request::server('HTTP_X_FORWARDED_PROTO') != 'http' ? true : '') }}">
+    </script>
 
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js" defer></script>
-    <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js" defer></script>
-    <script type="text/javascript" src="{{ asset('js/bootstrap-iconpicker.bundle.min.js', (Request::server('HTTP_X_FORWARDED_PROTO') != 'http' ? true : '')) }}" defer></script>
+    <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"
+        defer></script>
+    <script type="text/javascript"
+        src="{{ asset('js/bootstrap-iconpicker.bundle.min.js', Request::server('HTTP_X_FORWARDED_PROTO') != 'http' ? true : '') }}"
+        defer></script>
 
     <!-- Styles -->
     <link href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
-    <link href="{{ asset(mix('css/app.css'), (Request::server('HTTP_X_FORWARDED_PROTO') != 'http' ? true : '')) }}" rel="stylesheet">
+    <link href="{{ asset(mix('css/app.css'), Request::server('HTTP_X_FORWARDED_PROTO') != 'http' ? true : '') }}"
+        rel="stylesheet">
     <meta name="color-scheme" content="dark light">
 
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" />
-    <link rel="stylesheet" href="{{ asset('css/bootstrap-iconpicker.min.css', (Request::server('HTTP_X_FORWARDED_PROTO') != 'http' ? true : '')) }}" />
+    <link rel="stylesheet"
+        href="{{ asset('css/bootstrap-iconpicker.min.css', Request::server('HTTP_X_FORWARDED_PROTO') != 'http' ? true : '') }}" />
 
     @yield('customHead')
 
@@ -73,42 +82,65 @@
     </div>
 
     <!-- Botom Fixed Menu -->
-    <nav class="navbar fixed-bottom bg-light">
-        <div class="container-fluid">
-            <div class="navbar-expand w-100">
-                <ul class="navbar-nav justify-content-around nav-pills">
-                    @auth
-                        @include('components.navigation')
-                    @endauth
-                </ul>
+    @if (!session('dashboard'))
+        <nav class="navbar fixed-bottom bg-light fw-900 p-0"
+            style="z-index: 1056; height: 60px; font-size: 22px; padding-bottom: env(safe-area-inset-bottom);">
+            <div class="container-fluid p-0">
+                <div class="navbar-expand w-100">
+                    <ul class="navbar-nav justify-content-around nav-pills">
+                        @auth
+                            @include('components.navigation')
+                        @endauth
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    @endif
+
+    <!-- Full screen modal -->
+    @auth
+        @yield('modal')
+        <div class="modal" id="notifications" tabindex="-1" aria-labelledby="notifications" aria-hidden="true"
+            role="dialog">
+            <div class="modal-dialog modal-fullscreen-md-down">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">{{ __('simplehome.notification') }}
+                        </h5>
+                        <div class="btn-group">
+                            <a data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fas fa-ellipsis-h"></i>
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                                <li>
+                                    <a id="notification-control-load" class="btn btn-primary dropdown-item"
+                                        data-url="{{ route('notifications.read', ['notification_id' => 'all']) }}">
+                                        readAll
+                                    </a>
+                                </li>
+                                <li>
+                                    <a id="notification-control-load" class="btn btn-primary dropdown-item"
+                                        data-url="{{ route('notifications.delete', ['notification_id' => 'all']) }}">
+                                        deleteAll
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div id="notifications-list" data-url="{{ route('notifications.list') }}"></div>
+                    </div>
+                </div>
             </div>
         </div>
-    </nav>
-    <script src="{{ asset(mix('js/app.js'), (Request::server('HTTP_X_FORWARDED_PROTO') != 'http' ? true : '')) }}" defer></script>
-    <script defer>
-        $('body').on('click', 'div.control-relay', function(event) {
-            navigator.vibrate([10]);
-            thisObj = $(this);
-            thisObj.html("<div class=\"spinner-border text-primary\" role=\"status\"></div>");
-            console.log(thisObj.data("url"));
-            $.ajax({
-                type: 'POST',
-                url: thisObj.data("url"),
-                data: {
-                    _token: "{{ csrf_token() }}"
-                },
-                success: function(msg) {
-                    thisObj.html(msg.icon);
-                    thisObj.data("url", msg.url)
-                },
-                error: function() {
-                    //timeout
-                },
-                timeout: 3000,
-            });
-        });
-    </script>
+    @endauth
+
     @yield('beforeBodyEnd')
+    <script src="{{ asset(mix('js/utillities.js'), Request::server('HTTP_X_FORWARDED_PROTO') != 'http' ? true : '') }}">
+    </script>
+    <script src="{{ asset(mix('js/controls.js'), Request::server('HTTP_X_FORWARDED_PROTO') != 'http' ? true : '') }}">
+    </script>
 </body>
 
 </html>
