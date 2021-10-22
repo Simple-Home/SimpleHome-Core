f31f95d582125c87d24f7f0f12838a28fa27b2d3
GamerClassN7 <gamerclassn7@gmail.com>
2021-10-22 15:06:39 +0200

    Fixes

---
+++ resources/js/bootstrap-Iconspicker-laravel_notSubmit.js
@@ -0,0 +1,41 @@
+window.addEventListener('load', function () {
+    document.querySelectorAll('.iconButton').forEach(item => {
+        item.querySelector('input').setAttribute("name", "icon");
+        item.onclick = function () {
+            var popName = item.getAttribute("aria-describedby");
+            var pop = document.querySelector("#" + popName);
+            pop.querySelector(".btn-next").addEventListener('mouseup', function() {  
+                setTimeout( function () {
+                    pop.querySelectorAll('.btn-icon').forEach(item2 => {
+                        item2.addEventListener('mouseup', function() {
+                            item.querySelector('input').value = item2.value;
+                        });
+                    });
+                }, 1);
+            });
+            pop.querySelector(".btn-previous").addEventListener('mouseup', function() {
+                setTimeout( function () {
+                    pop.querySelectorAll('.btn-icon').forEach(item2 => {
+                        item2.addEventListener('mouseup', function() {
+                            item.querySelector('input').value = item2.value;
+                        });
+                    });
+                }, 1);
+            });
+            pop.querySelector(".search-control").addEventListener('input', function() {
+                setTimeout( function () {
+                    pop.querySelectorAll('.btn-icon').forEach(item2 => {
+                        item2.addEventListener('mouseup', function() {
+                            item.querySelector('input').value = item2.value;
+                        });
+                    });
+                }, 1000);
+            });
+            pop.querySelectorAll('.btn-icon').forEach(item2 => {
+                item2.addEventListener('mouseup', function() {
+                    item.querySelector('input').value = item2.value;
+                });
+            });
+        };
+    });
+});
