c579c5d35bc8ea1081d96362f36be6f4b3963499
JonatanRek <vasek@steelants.cz>
2021-10-26 18:25:31 +0200

    Visual Tweeks

--- app/Forms/NotificationForm.php
+++ app/Forms/NotificationForm.php
@@ -11,24 +11,36 @@ class NotificationForm extends Form
     public function buildForm()
     {
         $this
-            ->add('mail', Field::CHECKBOX, [
-                'label' => __('simplehome.notifyMail'),
-                'attr' => ['value' => 'mail']
-            ])
-            ->add('database', Field::CHECKBOX, [
-                'label' => __('simplehome.notifyDatabase'),
-                'attr' => ['value' => 'database']
-            ])
-            ->add('firebase', Field::CHECKBOX, [
-                'label' => __('simplehome.notifyFCM'),
-                'attr' => ['value' => 'firebase']
-            ])
-            ->add('saveNotify', Field::BUTTON_SUBMIT, [
-                'label' => __('simplehome.saveNotify'),
-                'attr' => [
-                    'class' => 'btn btn-primary  btn-block'
-                    ],
-                'wrapper' => ['class' => 'd-grid gap-2']
-            ]);
+        ->add('mail', Field::CHECKBOX, [
+            'label' => __('simplehome.notifyMail'),
+            'attr' => [
+                'value' => 'mail',
+                'class'=>'form-check-input bg-light',
+            ],
+        ])
+        ->add('database', Field::CHECKBOX, [
+            'label' => __('simplehome.notifyDatabase'),
+            'attr' => [
+                'value' => 'database',
+                'class'=>'form-check-input bg-light',
+            ],
+        ])
+        ->add('firebase', Field::CHECKBOX, [
+            'label' => __('simplehome.notifyFCM'),
+            'attr' => [
+                'value' => 'firebase',
+                'class'=>'form-check-input bg-light',
+            ],
+        ])
+        ->add('saveNotify', Field::BUTTON_SUBMIT, [
+            'label' => __('simplehome.saveNotify'),
+            'attr' => [
+                'class' => 'btn btn-primary  btn-block'
+            ],
+            'wrapper' => [
+                'class' => 'd-grid gap-2'
+            ],
+        ]);
     }
 }
+            
