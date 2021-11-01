c28e9bfa4d02f99fb5de315f478864cdcc70c1ff
JonatanRek <vasek@steelants.cz>
2021-11-01 08:39:54 +0100

    Job Exception Generation FIx

--- app/Jobs/CleanRecords.php
+++ app/Jobs/CleanRecords.php
@@ -12,6 +12,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
 
 class CleanRecords implements ShouldQueue
 {
