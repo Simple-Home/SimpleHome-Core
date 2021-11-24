aaa239bbbd28f9f146ee2faad4a04d56077f019e
JonatanRek <vasek@steelants.cz>
2021-11-24 12:02:23 +0100

    Clean Cache after Save

--- app/Http/Controllers/EnvController.php
+++ app/Http/Controllers/EnvController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Artisan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 use Kris\LaravelFormBuilder\FormBuilder;
 
@@ -52,6 +53,8 @@ class EnvController extends Controller
 
         // Artisan::call('cache:clear');
         // Artisan::call('config:cache');
+
+        Cache::flush();
 
         //die();
 
