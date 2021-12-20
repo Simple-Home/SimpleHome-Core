8cea6b4eb7955880d43e5ae422b6bbd6750a477c
GamerClassN7 <gamerclassn7@gmail.com>
2021-12-20 12:52:00 +0100

    Controls Detail wisual tweaks

--- resources/views/controls/components/subnavigation.blade.php
+++ resources/views/controls/components/subnavigation.blade.php
@@ -5,13 +5,4 @@
                 data-url="{{ route('controls.ajax.list', ['room_id' => $room->id]) }}">{{ $room->name }}</div>
         </li>
     @endforeach
-    @if (!session('dashboard'))
-        <li class="nav-item my-auto">
-            <!-- Button trigger modal -->
-            <button type="button" class="btn btn-link" data-bs-toggle="modal" data-bs-target="#staticBackdrop"
-                title="{{ __('simplehome.room.create') }}">
-                <i class="fas fa-plus"></i>
-            </button>
-        </li>
-    @endif
 @endif
